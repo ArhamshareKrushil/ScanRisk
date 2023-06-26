f591054d0c3597ac672a7f5f2f457d1bf44d8477
Sonal <sonalnikhar36@gmial.com>
2023-06-26 15:00:04 +0530

    ui

---
+++ Application/Views/proxyModels/modelSortfilterPOTW.py
@@ -0,0 +1,158 @@
+from PyQt5.QtCore import *
+
+class ProxyModel (QSortFilterProxyModel): #Custom Proxy Model
+    def __init__(self):
+        super(ProxyModel,self).__init__()
+        self.cllientCode =''
+        self.symbol=''
+        self.token=''
+        self.Neg=False
+
+    # def filterAcceptsRow(self, row, parent):
+    #     # print('filter')
+    #     if(self.cllientCode=='' and self.symbol==''):
+    #         return True
+    #
+    #     elif(self.symbol==''):
+    #         if (self.sourceModel().index(row, 0, parent).data() == self.cllientCode):
+    #             return True
+    #         else:
+    #             return False
+    #     elif (self.cllientCode == ''):
+    #         if (self.symbol in self.sourceModel().index(row, 4, parent).data()):
+    #             return True
+    #         else:
+    #             return False
+    #
+    #     else:
+    #         if(self.sourceModel().index(row, 0, parent).data() == self.cllientCode and self.symbol in self.sourceModel().index(row, 4, parent).data()):
+    #             # print(self.cllientCode,self.symbol)
+    #             return True
+    #         else:
+    #             return False
+    def filterAcceptsRow1(self, row, parent):
+        # print('filter')
+
+
+        if(self.cllientCode=='' and self.symbol=='' and self.token==''):
+            return True
+
+
+        elif (self.cllientCode == '' and self.symbol==''):
+            if (self.sourceModel().index(row, 2, parent).data() == self.token):
+                return True
+            else:
+                return False
+        elif(self.cllientCode == '' and self.token==''):
+            if (self.symbol in self.sourceModel().index(row, 4, parent).data()):
+                return True
+            else:
+                return False
+
+            # else:
+            #     if (self.sourceModel().index(row, 2, parent).data() == self.token and self.symbol in self.sourceModel().index(row, 4, parent).data()):
+            #         return True
+            #     else:
+            #         return False
+
+
+        elif (self.symbol == ''):
+            if (self.cllientCode == ''):
+                if (self.sourceModel().index(row, 2, parent).data() == self.token):
+                    return True
+                else:
+                    return False
+            elif (self.token == ''):
+                if (self.sourceModel().index(row, 0, parent).data() == self.cllientCode):
+                    return True
+                else:
+                    return False
+            else:
+                if (self.sourceModel().index(row, 2, parent).data() == self.token and self.sourceModel().index(row, 0, parent).data() == self.cllientCode):
+                    return True
+                else:
+                    return False
+
+        elif (self.token == ''):
+            if (self.cllientCode == ''):
+                if (self.symbol in self.sourceModel().index(row, 4, parent).data()):
+                    return True
+                else:
+                    return False
+            elif (self.symbol == ''):
+                if (self.sourceModel().index(row, 0, parent).data() == self.cllientCode):
+                    return True
+                else:
+                    return False
+            else:
+                if (self.sourceModel().index(row, 0, parent).data() == self.cllientCode and self.symbol in self.sourceModel().index(row, 4, parent).data()):
+                    return True
+                else:
+                    return False
+
+        else:
+            if(self.sourceModel().index(row, 0, parent).data() == self.cllientCode and self.symbol in self.sourceModel().index(row, 4, parent).data() and self.sourceModel().index(row, 2, parent).data() == self.token):
+                # print(self.cllientCode,self.symbol)
+                return True
+            else:
+                return False
+
+    def filterAcceptsRow(self, row, parent):
+        client = self.sourceModel().index(row, 0, parent).data()
+        symbol = self.sourceModel().index(row, 4, parent).data()
+        token = self.sourceModel().index(row, 2, parent).data()
+        Qty = self.sourceModel().index(row, 15, parent).data()
+        # print(type(client),type(symbol),type(token))
+
+        if(self.token != ''):
+
+            # print(self.token,token,type(self.token),type(token))
+
+            if(int(self.token)==token):
+                return True
+            else:
+                return False
+
+
+
+        else:
+            if(self.cllientCode=='' and self.symbol == ''):
+                if (self.Neg == True):
+                    if (int(Qty) < 0):
+                        return True
+                    else:
+                        return False
+                return True
+            elif(self.symbol == '' and self.cllientCode==client):
+                if (self.Neg == True):
+                    if (int(Qty) < 0):
+                        return True
+                    else:
+                        return False
+                return True
+            elif(self.symbol.lower() in symbol.lower() and self.cllientCode==''):
+                if (self.Neg == True):
+                    if (int(Qty) < 0):
+                        return True
+                    else:
+                        return False
+                return True
+            elif(self.symbol.lower() in symbol.lower() and self.cllientCode==client):
+                if (self.Neg == True):
+                    if (int(Qty) < 0):
+                        return True
+                    else:
+                        return False
+                return True
+            else:
+                return False
+
+
+    def setClientCode(self,code):
+        self.cllientCode =code
+
+    def setSymbol(self,symbol):
+        self.symbol = symbol
+
+    def setToken(self,token):
+        self.token=token
